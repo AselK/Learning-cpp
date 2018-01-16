0cf3b739f4785fdf34ce3dea846626011dda77ac
AselK <asel.nurmuhanbetova@gmail.com>
2018-01-16 18:42:39 +0300

    adding a file with my homework

---
+++ sortArrayHomework.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<algorithm>
+#include<ctime>
+ 
+using namespace std;
+ 
+int main()
+{
+    srand(time(0));
+    const int N=100;
+    int a[N];
+    for(int i=0;i<N;i++) 
+    {
+        a[i]=rand()%100;
+        cout<<a[i]<<" ";
+    }
+    cout<<endl<<"=================="<<endl;
+    sort(&a[0],&a[N]);//сортировка
+    for(int i=0;i<N;i++) cout<<a[i]<<" ";
+    cout<<endl;
+    getchar();
+    return 0;
+}
