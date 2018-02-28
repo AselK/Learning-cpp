f04cbc38b511b6f866ea06d4e202fac14f836c24
AselK <asel.nurmuhanbetova@gmail.com>
2018-02-28 19:02:25 +0300

    making variables private; adding methods to get and set variables

--- class/classTask1.cpp
+++ class/classTask1.cpp
@@ -5,19 +5,28 @@
 class Rectangle
 {
 public:
-	double h;
-	double w;
 	double area()
 	{
-		return w * h;
+		return width_ * height_;
 	}
+	void setHeight(double height)
+	{
+		height_ = height;
+	}
+	void setWidth(double width)
+	{
+		width_ = width;
+	}
+private:
+	double height_;
+	double width_;
 };
 
 
 int main()
 {
 	Rectangle rect1;
-	rect1.h = 10;
-	rect1.w = 3;
+	rect1.setHeight(10);
+	rect1.setWidth(3);
 	std::cout << rect1.area();
 }
