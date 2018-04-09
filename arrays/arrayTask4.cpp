8f23445556524d534e77370a352459e7acbe79ff
AselK <asel.nurmuhanbetova@gmail.com>
2018-04-09 22:12:35 +0300

    adding array

---
+++ arrays/arrayTask4.cpp
@@ -0,0 +1,12 @@
+//Выдать все элементы массива на экран
+
+#include <iostream>
+
+int main()
+{
+	int arr[5] = {56,89,34,22,90};
+	for ( int i = 0; i < 5; i++)
+	{
+		std::cout << arr[i] <<std::endl;
+	}
+}
