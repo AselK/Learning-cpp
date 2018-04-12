75087d1ff4cbaf9bae26ce590eb0896564e9c208
AselK <asel.nurmuhanbetova@gmail.com>
2018-04-12 19:11:25 +0300

    change some code

--- arrays/ArraySum1.cpp
+++ arrays/ArraySum1.cpp
@@ -2,31 +2,30 @@
 
 int main()
 {
-	int array1[100] = {6,5,7};
-	int array2[100] = {9,8,7};
+	int array1[100] = {0};
+	array1[0] = 6;
+	array1[1] = 5;
+	array1[2] = 7;
+	array1[3] = 8;
+	int array2[100] = {0};
+	array2[0] = 9;
+	array2[1] = 8;
+	array2[2] = 7;
+	array2[3] = 6;
 	int array3[100] = {0};
+	int tmp = 0;
+	for (int i = 0; i < 4; i++)
+	{
+		tmp = array1[i] + array2[i] + tmp;
+		array3[i] = tmp % 10; 
+		tmp = tmp / 10; 
+		
+	}
 	
+	array3[4] = tmp;
 	
-	int tmp = array1[0] + array2[0]; // 15
-	array3[0] = tmp % 10; // 5
-	tmp = tmp / 10; // 1
-	std::cout << array3[0];
-	
-	tmp = array1[1] + array2[1] + tmp; //14
-	//std::cout << tmp <<std::endl;
-	array3[1] = tmp % 10; //4
-	//std::cout << array3[1] << std::endl;
-	tmp = tmp / 10; //1
-	//std::cout << tmp << std::endl;
-	std::cout << array3[1];
-	
-	tmp = array1[2] + array2[1] + tmp; //15
-	//std::cout << tmp <<std::endl;
-	array3[2] = tmp % 10; //5
-	//std::cout << array3[1] << std::endl;
-	tmp = tmp / 10; //1
-	//std::cout << tmp << std::endl;
-	std::cout << array3[2];
-	array3[3] = tmp;
-	std::cout << tmp << std::endl;
+	for (int i = 4; i >= 0; i--)
+	{
+		std::cout << array3[i];
+	}
 }
